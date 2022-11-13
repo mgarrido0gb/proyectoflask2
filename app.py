6f052df6a316516b57191f9824b4665aee9e8511
mgarrido0gb <contacto.garridomatias@gmail.com>
2022-11-13 17:11:27 -0400

    c2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def ping():
+def inicio():
     return jsonify({"response":"Hola Mundo"})
 
 if __name__ == '__main__':
