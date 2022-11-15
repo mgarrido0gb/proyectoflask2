d5639f0ec19031d20e9a6d1dd8055a8ed8d6cd38
mgarrido0gb <contacto.garridomatias@gmail.com>
2022-11-15 10:03:57 -0400

    Genera txt con los campos separados por "|"

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #def inicio():
 #    return jsonify({"response":"Hola Mundo"})
 
-sitioweb = 'https://www.culturagenial.com/es/peliculas-recomendadas/'
+sitioweb = 'https://www.bcn.cl/historiapolitica/presidentes_de_la_republica/index.html'
 respuesta = requests.get(sitioweb)
 contenido =respuesta.text
 
@@ -16,14 +16,21 @@ contenido =respuesta.text
 soup =  BeautifulSoup(contenido,'lxml')
 #print(soup.prettify())
 
-caja = soup.find('article',class_='article')
+todo = soup.find_all('div',class_='container')
 
-titulo = caja.find('h3').get_text()
-transcript = caja.find('h3').get_text(strip=True, separator=' ')
-print(titulo)
-with open(f'{titulo}.txt', 'w', encoding='utf-8') as file:
-    file.write(transcript)
- 
+nombres = soup.find_all('h5',class_='seleccionRS')
+
+
+titulotxt = list()
+for listado in nombres:
+    resultado = print(listado.text.replace(' ','|'))
+    titulotxt.append(listado.text)
+    
+todo = list(map(str,titulotxt))
+resultado = ''.join(todo)
+    
+with open('presidentes.txt', 'w', encoding='utf-8') as file:
+   file.write(resultado.replace(' ','|'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=4000,debug=True) 
